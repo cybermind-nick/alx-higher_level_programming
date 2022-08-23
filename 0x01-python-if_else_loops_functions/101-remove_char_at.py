f85aa16dcc52641a8672f918d4d46e5005e62329
Nicholas <nickifeajika@gmail.com>
2022-08-23 22:39:30 +0100

    erase index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n == 0:
-        print("{}".format(str[1:]))
-    else if n == len(str) - 1:
-        print("{}".format(str[:n]))
+    if n >= 0:
+        return str[:n] + [n+1:]
     else:
-        print("{}".format(str[:n] + [n+1:]))
+        return str
