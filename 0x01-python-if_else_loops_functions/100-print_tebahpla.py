9d543fee480b58f5d0586dc42546025a285c045a
Nicholas <nickifeajika@gmail.com>
2022-08-23 22:20:43 +0100

    alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,8 @@
 state = True
 for i in range(123, 96, -1):
     if state:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
     else:
-        print(chr(i).upper(), end="")
+        print("{}".format(chr(i).upper()), end="")
 
     state = not state  # toggle
