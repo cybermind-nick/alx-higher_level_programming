f8906602870e869f43035858c45e6c3e58e8d952
Nicholas <nickifeajika@gmail.com>
2022-08-25 18:13:43 +0100

    file arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    userArgs = argv[1:]  # ignoring the filename with idx 0
+    size = len(userArgs)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) != 1 else "argument",
+                 "." if (size) == 0 else ":"))
+    for idx, arg in enumerate(userArgs):
+        print("{:d}: {:s}".format(idx + 1, arg))
