bb3a17dc43be0d7fd2f25688498fed30f2e495f7
Nicholas <nickifeajika@gmail.com>
2022-08-29 10:54:21 +0100

    insert element at list index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    list_copy = my_list.copy()
+    if idx >= 0 and idx < len(my_list):
+        list_copy[idx] = element
+    return list_copy
