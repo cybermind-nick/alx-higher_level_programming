30bc4a76989cea83f4bc35ff98bbadee67310b9d
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-09-12 18:24:48 +0100

    Zero division exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,4 +7,4 @@ def safe_print_division(a, b) -> int:
         return None
     finally:
         print("Inside result: {}".format(result))
-        print("{:d} / {:d} = {}".format(a, b, result))
+        return result
