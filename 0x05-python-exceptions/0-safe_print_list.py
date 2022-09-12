55ce422d2b3db63ca32fa907f92ecd0142eb4e34
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-09-12 14:52:21 +0100

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0) -> int:
+    elements = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            elements += 1
+        except:
+            print()
+            return elements
+    print()
+    return elements
