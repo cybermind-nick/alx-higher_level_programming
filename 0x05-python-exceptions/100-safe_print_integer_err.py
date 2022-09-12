ba9e6b8748bfa272e32a7d909c6953327f719b25
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-09-12 23:07:24 +0100

    safe print int stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value) -> bool:
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        stderr.write("Exception {} ".format(e))
+        return False
