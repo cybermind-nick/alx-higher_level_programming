9fc48fdfc6fe8f1f86340b12a7f8e149fc45f9ee
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-09-12 17:26:31 +0100

    naked exception fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0) -> int:
         try:
             print(my_list[i], end='')
             elements += 1
-        except RangeError:
+        except IndexError:
             print()
             return elements
     print()
