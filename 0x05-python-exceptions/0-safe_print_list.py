c04e04ee4009ea2f558794536161cfc22d2a24b2
Nicholas <nickifeajika@gmail.com>
2022-09-12 17:19:31 +0100

    RangeError Exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0) -> int:
         try:
             print(my_list[i], end='')
             elements += 1
-        except:
+        except RangeError:
             print()
             return elements
     print()
