0984446db3c3babe8df7421d969854f16af232ba
Nicholas Ifeajika <nicholas@DESKTOP-PRBDBM7.localdomain>
2022-09-19 19:46:02 +0100

    Areas and Perimeters

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,4 +57,7 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
